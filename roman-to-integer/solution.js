38b43066450cc80e067422614031440cf4e50fde
murilo <murilosalomao95@gmail.com>
2023-05-29 22:02:41 -0300

    add roman-to-integer solution

---
+++ roman-to-integer/solution.js
@@ -0,0 +1,26 @@
+// O(n)
+var romanToInt = function (s) {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (i + 1 < s.length && values[s[i + 1]] > values[s[i]]) {
+      sum += values[s[i + 1]] - values[s[i]];
+      i++;
+    } else {
+      sum += values[s[i]];
+    }
+  }
+
+  return sum;
+};
+
+romanToInt("MCMXCIV");
