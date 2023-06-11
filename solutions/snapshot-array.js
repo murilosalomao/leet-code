b1ae01809b2416bd1948c87f3477bca8cf26a022
murilo <murilosalomao95@gmail.com>
2023-06-11 08:55:25 -0300

    minor changes to snapshot-array solution

--- solutions/snapshot-array.js
+++ solutions/snapshot-array.js
@@ -1,27 +1,26 @@
 class SnapshotArray {
-  constructor(length) {
+  constructor(_length) {
     this.arr = {};
     this.snaps = {};
     this.snapId = 0;
     this.hasBeenChanged = false;
   }
+
+  set = function (index, val) {
+    this.arr[index] = val;
+    this.hasBeenChanged = true;
+  };
+
+  snap = function () {
+    this.snaps[this.snapId] = this.hasBeenChanged
+      ? { ...this.arr }
+      : this.snaps[this.snapId - 1];
+    this.hasBeenChanged = false;
+    return this.snapId++;
+  };
+
+  get = function (index, snap_id) {
+    if (!this.snaps[snap_id]) return null;
+    return this.snaps[snap_id][index] || 0;
+  };
 }
-
-SnapshotArray.prototype.set = function (index, val) {
-  this.arr[index] = val;
-  this.hasBeenChanged = true;
-};
-
-SnapshotArray.prototype.snap = function () {
-  this.snaps[this.snapId] = this.hasBeenChanged
-    ? { ...this.arr }
-    : this.snaps[this.snapId - 1];
-  this.snapId++;
-  this.hasBeenChanged = false;
-  return this.snapId - 1;
-};
-
-SnapshotArray.prototype.get = function (index, snap_id) {
-  if (!this.snaps[snap_id]) return null;
-  return this.snaps[snap_id][index] || 0;
-};
